bb0fcec5cb47a1184903a4080dedaa68eefdc7fa
tomDesktop <tomrach11@gmail.com>
2020-11-24 00:20:42 -0500

    codility

--- JavaScript/Codility SmoothStack.js
+++ JavaScript/Codility SmoothStack.js
@@ -1,14 +1,13 @@
-// https://app.codility.com/c/feedback/7HUUBC-GBX/
 
 function solution(A) {
     var n = A.length;
     var result = 0;
     var i;
-    for (i = 0; i < n; i++) {
+    for (i = 0; i < n - 1; i++) {
         if (A[i] == A[i + 1])
             result = result + 1;
     }
-    var r = 0;
+    var r = -1;
     for (i = 0; i < n; i++) {
         var count = 0;
         if (i > 0) {
@@ -27,3 +26,12 @@ function solution(A) {
     }
     return result + r;
 }
+
+console.log(solution([0, 0, 1, 1])); // 2
+console.log(solution([ 1, 1, 0, 1, 0, 0, 1, 1 ])); // 5
+console.log(solution([ 1, 1, 1, 1, 1, 0, 1, 1 ])); // 7
+console.log(solution([ 1, 0, 1])); // 2
+console.log(solution([ 0, 1, 0])); // 2
+console.log(solution([ 1, 1, 0, 0, 0])); // 3
+console.log(solution([0])); // 0
+console.log(solution([1, 1, 1])); // 1
--- JavaScript/Run Noode/run.js
+++ JavaScript/Run Noode/run.js
@@ -1,20 +1,40 @@
 
-// Last Index
-// Write a method last_index that takes in a string and a character. 
-//The method should return the last index where the character can be found in the string.
-function last_index(str, char) {
-    //loop from back
-    //find the index of the letter
-    //return it
+function solution(A) {
+    var n = A.length;
+    var result = 0;
+    var i;
 
-    for (let i = str.length; i >= 0; i--) {
-        if (str[i] === char) {
-            return i;
-        }
+    //loop to make total adjacent counted
+    for (i = 0; i < n - 1; i++) {
+        if (A[i] == A[i + 1])
+            result = result + 1;
     }
-
+    //
+    var r = -1;
+    for (i = 0; i < n; i++) {
+        var count = 0;
+        if (i > 0) {                    //check if there any on the left
+            if (A[i - 1] != A[i])       //see if current equal to left
+                count = count + 1;
+            else
+                count = count - 1;
+        }
+        if (i < n - 1) {                //check if there is any on the right
+            if (A[i + 1] != A[i])       //see if current equal to right
+                count = count + 1;
+            else
+                count = count - 1;
+        }
+        r = Math.max(r, count);         //r does not need to change if count is less than r ex. if it already -1 count is -2 r should not change to -2
+    }
+    return result + r;
 }
-console.log(last_index("abca", "a")       ); //#=> 3
-console.log(last_index("mississipi", "i") ); //#=> 9
-console.log(last_index("octagon", "o")    ); //#=> 5
-console.log(last_index("programming", "m")); //#=> 7
+
+console.log(solution([0, 0, 1, 1])); // 2
+console.log(solution([ 1, 1, 0, 1, 0, 0, 1, 1 ])); // 5
+console.log(solution([ 1, 1, 1, 1, 1, 0, 1, 1 ])); // 7
+console.log(solution([ 1, 0, 1])); // 2
+console.log(solution([ 0, 1, 0])); // 2
+console.log(solution([ 1, 1, 0, 0, 0])); // 4
+console.log(solution([0])); // 0
+console.log(solution([1, 1, 1])); // 1
